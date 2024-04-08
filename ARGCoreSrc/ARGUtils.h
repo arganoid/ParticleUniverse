434b7d7a5a8fcc00a5d7bd3dac83a701c4a0c4a9
Andrew Gillett <arg@arganoid.com>
2024-04-08 16:46:01 +0100

    Tidy up ARGUtils: use standard concurreny library, make getrandom a function, remove unused functions, don't do using in header file

--- ARGCoreSrc/ARGUtils.h
+++ ARGCoreSrc/ARGUtils.h
@@ -5,36 +5,17 @@
 #include <string>
 #include <cmath>
 
-using std::string;
-
-static const char argUtilsBufLength = 40;
-static char argUtilsBuf[argUtilsBufLength];
-
-inline string IntToString(int _int)
+inline int getrandom(int min, int max)
 {
-	sprintf_s(argUtilsBuf, "%d", _int);
-	return argUtilsBuf;
+	return (rand() % ((max + 1) - min)) + min;
 }
-
-inline string FloatToString(float _f)
-{
-	sprintf_s(argUtilsBuf, "%f", _f);
-	return argUtilsBuf;
-}
-
-// Macro to get a random integer within a specified range
-#define getrandom( min, max ) ((rand() % (int)(((max)+1) - (min))) + (min))
 
 inline float FRand(float minVal, float maxVal)
 {
 	return minVal + ((float)rand() / RAND_MAX) * (maxVal - minVal);
 }
 
-void argDebug(string const& _str);
-void argDebugf(string _str, ...);
-void argDebugfT(int _task, string _str, ...);
+void argDebug(std::string const& _str);
+void argDebugf(std::string _str, ...);
 
-string stringFormat(string _str, ...);
-
-// From techbytes.ca
-bool argFileExists(char* _strFilename);
+std::string stringFormat(std::string _str, ...);
