c10d28beca056d194cc320522a868d00bf8db928
Andrew Gillett <arg@arganoid.com>
2024-04-08 16:48:35 +0100

    Tidy up ARGUtils: use standard concurreny library, make getrandom a function, remove unused functions, don't do using in header file

--- ARGCoreSrc/ARGUtils.cpp
+++ ARGCoreSrc/ARGUtils.cpp
@@ -1,14 +1,10 @@
-#include <concrt.h>
-#include <ppltasks.h>
-
-#include <sys\stat.h>
-
 #include <map>
 #include <iostream>
 #include <iomanip>
 #include <fstream>
 #include <vector>
 #include <sstream>
+#include <mutex>
 
 #include <stdarg.h>
 
@@ -28,7 +24,7 @@ static char buf[1048576];	// todo stop using c strings
 
 //static vector<string> dbgOutput;
 
-static concurrency::critical_section g_dbgMutex;
+static mutex g_logMutex;
 
 void argDebug(string const& _str)
 {
@@ -36,7 +32,7 @@ void argDebug(string const& _str)
 	// OutputDebugString seems to be unable to keep up with the volume of text in certain circumstances, and will thus
 	// skip text or fail to output it at all. Always check the log file, which has no such known problems.
 #if DEBUG_ENABLED
-	concurrency::critical_section::scoped_lock lock(g_dbgMutex);	// needed? http://stackoverflow.com/questions/22539282/is-msvcrts-implementation-of-fprintf-thread-safe
+	scoped_lock lock(g_logMutex);	// needed? http://stackoverflow.com/questions/22539282/is-msvcrts-implementation-of-fprintf-thread-safe
 
 	//sprintf_s(buf, "%d: ", g_advanceCounter);
 	//OutputDebugString( string(buf + _str + "\n").c_str() );
@@ -46,14 +42,6 @@ void argDebug(string const& _str)
 		g_logFile.open("argcore.log");
 	}
 
-	//fprintf_s(g_logFile, string(buf + _str + "\n").c_str());
-	//fflush(g_logFile);
-
-	// https://social.msdn.microsoft.com/Forums/vstudio/en-US/9244b4ae-030d-475b-b31f-8da6794f884e/how-to-get-the-index-of-virtual-processor-or-the-context-in-ppl?forum=parallelcppnative
-	// GetVirtualProcessorId probably not reliable
-	//const signed id = (signed)concurrency::Context::CurrentContext()->GetVirtualProcessorId();
-
-	//g_logFile << g_advanceCounter << ":" << std::setw(2) << id << ": " << _str << endl << flush;
 	g_logFile << g_advanceCounter << ": " << _str << endl << flush;
 
 //	dbgOutput.push_back(_str);
@@ -69,69 +57,11 @@ void argDebugf(string _str, ...)	// can't use & with varargs
 	argDebug(buf);
 }
 
-void argDebugfT(int _task, string _str, ...)	// can't use & with varargs
-{
-	std::stringstream ostr;
-	ostr << std::setw(2) << _task << ": ";
-
-	va_list args;
-	va_start(args, _str);
-	vsprintf_s(buf, _str.c_str(), args);
-	va_end(args);
-
-	ostr << buf;
-	argDebug(ostr.str());
-}
-
-string stringFormat(string _str, ...)
+string stringFormat(string _str, ...)	// can't use & with varargs
 {
 	va_list args;
 	va_start(args, _str);
 	vsprintf_s(buf, _str.c_str(), args);
 	va_end(args);
 	return buf;
-}
-
-/*
-bool argKeypressed(int _key)
-{
-	static ALLEGRO_KEYBOARD_STATE state;
-	static std::map<int, bool> keyMap;
-	al_get_keyboard_state(&state);
-	if (!al_key_down(&state, _key) && keyMap[_key] == true)
-	{
-		keyMap[_key] = false;
-		return true;
-	}
-
-	keyMap[_key] = al_key_down(&state, _key);
-	return false;
-}
-*/
-
-// From techbytes.ca
-bool argFileExists(char* _strFilename)
-{
-	struct stat stFileInfo;
-	bool blnReturn;
-	int intStat;
-
-	// Attempt to get the file attributes
-	intStat = stat(_strFilename, &stFileInfo);
-	if (intStat == 0) {
-		// We were able to get the file attributes
-		// so the file obviously exists.
-		blnReturn = true;
-	}
-	else {
-		// We were not able to get the file attributes.
-		// This may mean that we don't have permission to
-		// access the folder which contains this file. If you
-		// need to do that level of checking, lookup the
-		// return values of stat which will give you
-		// more details on why stat failed.
-		blnReturn = false;
-	}
-
-	return(blnReturn);
 }
